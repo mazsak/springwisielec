d1e9ba52d4336074200d0fc4044b94c49ef05d4a
Andzik <smers20@wp.pl>
2019-06-11 14:40:37 +0200

    andzik

--- src/main/java/com/example/demo/controller/StartController.java
+++ src/main/java/com/example/demo/controller/StartController.java
@@ -1,20 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.Service.IUserService;
+import com.example.demo.Service.UserService;
 import com.example.demo.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
-public class StartController {
+public class StartController{
+
+    @Autowired
+    private IUserService userService;
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
-    @GetMapping("/language")
-    public String language() {
+    @PostMapping("/language")
+    public String language(@ModelAttribute("user") @Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()){
+            model.addAttribute("user", user);
+            return "/login";
+        }
+        userService.add(user);
         return "language";
     }
 
@@ -24,7 +40,7 @@ public class StartController {
         return "register";
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public String login(Model model) {
         model.addAttribute("user", new User());
         return "login";
