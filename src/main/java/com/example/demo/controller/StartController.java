76292f6836ed24bafeb217a0a34477f65285c4c6
Mateusz Samluk <mazsak97@gmail.com>
2019-06-11 13:32:26 +0200

    add new view

--- src/main/java/com/example/demo/controller/StartController.java
+++ src/main/java/com/example/demo/controller/StartController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StartController {
@@ -14,5 +16,11 @@ public class StartController {
     @GetMapping("/language")
     public String language() {
         return "language";
+    }
+
+    @PostMapping("/register")
+    public String register(Model model) {
+        //model.addAttribute("user", new User())
+        return "register";
     }
 }
